6dc9e54ca1d8203a0b671fae2ba1c991f56e3ed1
DabralPriyanshu <priyanshudabral07@gmail.com>
2026-03-01 22:31:47 +0530

    Added BadRequestError,NotFoundError and InternalServerError classes

---
+++ ProblemService/src/errors/NotFoundError.js
@@ -0,0 +1,17 @@
+const BaseError = require("./BaseError");
+const { StatusCodes } = require("http-status-codes");
+class NotFoundError extends BaseError {
+  constructor(resourceName, resourceValue) {
+    super(
+      "NotFound",
+      StatusCodes.NOT_FOUND,
+      `The requested resource: ${resourceName} with value ${resourceValue} not found`,
+      {
+        resourceName,
+        resourceValue,
+      },
+    );
+  }
+}
+
+module.exports = NotFoundError;
