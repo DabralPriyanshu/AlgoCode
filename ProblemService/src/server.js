5e7adac875e1bcf3a967d2c1ffde4e95deed03ad
DabralPriyanshu <priyanshudabral07@gmail.com>
2026-03-01 21:55:19 +0530

    Configure setup for environment varibales,added ping route and middlewares to parse request body

--- ProblemService/src/server.js
+++ ProblemService/src/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
-
+const { PORT } = require("./config/server.config");
 const app = express();
-
-app.listen(3000, () => {
-  console.log(`Server started at http://localhost:3000`);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get("/ping", (req, res) => {
+  return res.json({ message: "Problem Service is alive" });
+});
+app.listen(PORT, () => {
+  console.log(`Server started at http://localhost:${PORT}`);
 });
