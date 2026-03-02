35a55486638f1ca9d329fa8f5f69fce6d3dfce5d
DabralPriyanshu <priyanshudabral07@gmail.com>
2026-03-02 05:45:25 +0530

    Added global errorHandler middleware

--- ProblemService/src/server.js
+++ ProblemService/src/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const { PORT } = require("./config/server.config");
+const errorHandler = require("./utils/errorHandler");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.get("/ping", (req, res) => {
   return res.json({ message: "Problem Service is alive" });
 });
+app.use(errorHandler)
 app.listen(PORT, () => {
   console.log(`Server started at http://localhost:${PORT}`);
 });
