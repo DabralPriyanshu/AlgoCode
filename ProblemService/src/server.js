67c322b837b73ff42b1df2046c57612e6e1e4cb2
DabralPriyanshu <priyanshudabral07@gmail.com>
2026-03-02 06:24:57 +0530

    Added db connection setup

--- ProblemService/src/server.js
+++ ProblemService/src/server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const { PORT } = require("./config/server.config");
 const errorHandler = require("./utils/errorHandler");
+const connectToDB = require("./config/db.config");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.get("/ping", (req, res) => {
   return res.json({ message: "Problem Service is alive" });
 });
-app.use(errorHandler)
-app.listen(PORT, () => {
+app.use(errorHandler);
+app.listen(PORT, async () => {
+  await connectToDB();
   console.log(`Server started at http://localhost:${PORT}`);
 });
