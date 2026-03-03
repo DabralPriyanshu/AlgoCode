6c91bfc876dc1f58307a1195beeaae84bd15e39a
DabralPriyanshu <priyanshudabral07@gmail.com>
2026-03-03 10:15:54 +0530

    Added bullboard ui

---
+++ EvaluatorService/src/config/bullboard.config.ts
@@ -0,0 +1,15 @@
+import { createBullBoard } from "@bull-board/api";
+import { BullMQAdapter } from "@bull-board/api/bullMQAdapter";
+import { ExpressAdapter } from "@bull-board/express";
+
+import SampleQueue from "../queues/sampleQueue.js";
+
+const serverAdapter = new ExpressAdapter();
+serverAdapter.setBasePath("/ui");
+
+createBullBoard({
+  queues: [new BullMQAdapter(SampleQueue)],
+  serverAdapter,
+});
+
+export default serverAdapter;
--- EvaluatorService/src/server.ts
+++ EvaluatorService/src/server.ts
@@ -3,12 +3,17 @@ import ENV from "./config/server.config.js";
 import apiRoutes from "./routes/index.js";
 import addJobToSampleQueue from "./producers/sampleQueueProducer.js";
 import sampleWorker from "./workers/sampleWorker.js";
+import bullBoardAdapter from "./config/bullboard.config.js";
 const app: Express = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", apiRoutes);
+app.use("/ui", bullBoardAdapter.getRouter());
 app.listen(ENV.PORT, () => {
   console.log(`Server started at http://localhost:${ENV.PORT}`);
+  console.log(
+    `BullBoard dashboard running on: http://localhost:${ENV.PORT}/ui`,
+  );
   sampleWorker("SampleQueue");
   addJobToSampleQueue("SampleJob", { name: "Sanket", company: "Microsoft" }, 2);
   addJobToSampleQueue("SampleJob", { name: "Sarthak", company: "Google" }, 1);
