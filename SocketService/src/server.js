d3799bf59ecf219a4c0c6ae06558aae56d560b67
DabralPriyanshu <priyanshudabral07@gmail.com>
2026-03-07 07:51:41 +0530

    Added redis cache and socket events with sample frontend in html

--- SocketService/src/server.js
+++ SocketService/src/server.js
@@ -1,13 +1,39 @@
 const express = require("express");
 const { createServer } = require("node:http");
 const { Server } = require("socket.io");
+const Redis = require("ioredis");
 const app = express();
 const httpServer = createServer(app);
-
-const io = new Server(httpServer);
+const redisCache = new Redis();
+const io = new Server(httpServer, {
+  cors: { origin: "http://localhost:5500", methods: ["GET", "POST"] },
+});
 
 io.on("connection", (socket) => {
-  //
+  console.log("A user connected " + socket.id);
+  socket.on("setUserId", (userId) => {
+    console.log(userId);
+    redisCache.set(userId, socket.id);
+  });
+  socket.on("getConnectionId", async (userId) => {
+    console.log(userId);
+    const connId = await redisCache.get(userId);
+    socket.emit("connectionId", connId);
+  });
+});
+app.use(express.json());
+app.post("/sendPayload", async (req, res) => {
+  const { userId, payload } = req.body;
+  if (!userId || !payload) {
+    res.status(400).json({ message: "Invalid request" });
+  }
+  const socketId = await redisCache.get(userId);
+  if (socketId) {
+    io.to(socketId).emit("submissionPayloadResponse", payload);
+    res.status(200).send("Payload sent successfully");
+  } else {
+    res.status(404).send("User not connected");
+  }
 });
 
 httpServer.listen(3003, () => {
