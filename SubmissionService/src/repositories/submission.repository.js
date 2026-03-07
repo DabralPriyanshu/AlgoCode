9037a19d896e468eb56e49521ed44dec4fed7224
DabralPriyanshu <priyanshudabral07@gmail.com>
2026-03-07 20:22:21 +0530

    Added updateSubmission method to update submission status after code executed successfully

--- SubmissionService/src/repositories/submission.repository.js
+++ SubmissionService/src/repositories/submission.repository.js
@@ -12,5 +12,19 @@ class SubmissionRepository {
       throw error;
     }
   }
+  async updateSubmission(submissionId, updatedStatus) {
+    try {
+      console.log("submission id ", submissionId + " status ", updatedStatus);
+      const response = await this.model.findByIdAndUpdate(
+        submissionId,
+        { status: updatedStatus },
+        { new: true },
+      );
+      return response;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
 }
 module.exports = SubmissionRepository;
--- SubmissionService/src/workers/evaluationWorker.js
+++ SubmissionService/src/workers/evaluationWorker.js
@@ -1,18 +1,34 @@
 const { Worker } = require("bullmq");
 const redisConnection = require("../config/redis.config");
 const axios = require("axios");
-
+const SubmissionRepository = require("../repositories/submission.repository");
+const submissionRepository = new SubmissionRepository();
 function evaluationWorker(queueName) {
   new Worker(
     queueName,
     async (job) => {
       if (job.name === "EvaluationJob") {
-        console.log(job.data);
-        const response = await axios.post("http://localhost:3003/sendPayload", {
-          userId: job.data.userId,
-          payload: job.data,
-        });
-        console.log(response);
+        console.log("printing job data ", job.data);
+        console.log("printing job data ", job.data.response.status);
+        console.log("Submission repo->", submissionRepository);
+        try {
+          const res = await submissionRepository.updateSubmission(
+            job.data.submissionId,
+            job.data.response.status,
+          );
+          console.log("Response from db", res);
+          const response = await axios.post(
+            "http://localhost:3003/sendPayload",
+            {
+              userId: job.data.userId,
+              payload: job.data,
+            },
+          );
+          console.log(response);
+        } catch (error) {
+          console.log(error);
+          throw error;
+        }
       }
     },
     { connection: redisConnection },
